08bf514d995642f6826867fc3782f943027ca2e8
han <coolboyhan@163.com>
2017-06-09 10:07:46 +0800

    new network

---
+++ network/端口流量重定向/network_intetface.py
@@ -0,0 +1,35 @@
+import sys
+import socket
+import fcntl
+import struct
+import array
+#fcntl在posix接口，在windows下没有，脚本需要运行在linux下。
+
+SIOCGIFCONF = 0x8912
+STUCT_SIZE_32 = 32
+STUCT_SIZE_64 = 40
+PLATFORM_32_MAX_NUMBER = 2**32
+DEFAULT_INTERFACES = 8
+
+def list_interfaces():
+    interfaces = []
+    max_interfaces = DEFAULT_INTERFACES
+    is_64bits = sys.maxsize > PLATFORM_32_MAX_NUMBER
+    struct_size = STUCT_SIZE_64 if is_64bits else STUCT_SIZE_32
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    while True:
+        bytes = max_interfaces * struct_size
+        interface_names = array.array('B','\0'*bytes)
+        sock_info = fcntl.ioct1(sock.fileno(),SIOCGIFCONF,struct.pack('iL',bytes,interface_names.buffer_info()[0]))
+        outbytes = struct.unpack('iL',sock_info)[0]
+        if outbytes == bytes:
+            max_interfaces *= 2
+        else:
+            break
+        namestr = interface_names.tostring()
+        for i in range(0,outbytes,struct_size):
+            interfaces.append((namestr[i:i+16].split('\0',1)[0]))
+        return interfaces
+    if __name__=='__main__':
+        interfaces = list_interfaces()
+        print("this machine has %s network interfaces:%s."%(len(interfaces),interfaces))
